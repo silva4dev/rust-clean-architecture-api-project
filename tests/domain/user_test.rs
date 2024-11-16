84e1361b6ba2ab057dbf9df9b5c538cb9f94f35a
silva4dev <web4dev.lucas@outlook.com>
2024-11-16 02:39:44 -0300

    test: :test_tube: add user entity

---
+++ tests/domain/user_test.rs
@@ -0,0 +1,23 @@
+use rust_clean_architecture_api_project::domain::entities::user::User;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_user() {
+        let sut = User {
+            id: 1,
+            name: "John Doe".to_string(),
+            email: Some("johndoe@example.com".to_string()),
+            phone: "(19)99281-9212".to_string(),
+            address: "XYZ ABC Tower".to_string()
+        };
+
+        assert_eq!(sut.id, 1);
+        assert_eq!(sut.name, "John Doe");
+        assert_eq!(sut.email.unwrap(), "johndoe@example.com");
+        assert_eq!(sut.phone, "(19)99281-9212");
+        assert_eq!(sut.address, "XYZ ABC Tower")
+    }
+}
