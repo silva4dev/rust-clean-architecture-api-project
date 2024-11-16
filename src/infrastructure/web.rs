d1d103fe275c9f1385e8564fa64233377fa672f7
silva4dev <web4dev.lucas@outlook.com>
2024-11-16 02:38:59 -0300

    feat: :sparkles: export files

---
+++ src/infrastructure/web.rs
@@ -0,0 +1,21 @@
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use log::info;
+use crate::presentation::routes::user_routes::routes;
+use crate::infrastructure::repositories::postgres_user_repository::PostgresUserRepository;
+
+pub async fn run() -> std::io::Result<()> {
+    let repo = PostgresUserRepository::new();
+    let app_data = web::Data::new(repo);
+
+    info!("Starting server...");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_data.clone()) 
+            .wrap(Logger::default()) 
+            .configure(routes)
+    })
+    .bind("0.0.0.0:4000")? 
+    .run() 
+    .await
+}
